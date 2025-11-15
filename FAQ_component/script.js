7095296cd894fb1eb05efaab52de665432ab5a9d
drishti-gupta0704 <drishti.gupta070424@gmail.com>
2025-11-15 22:07:34 +0530

    adding more questions and arrow rotation on toggle

--- FAQ_component/script.js
+++ FAQ_component/script.js
@@ -1,16 +1,16 @@
-
 const questions = document.querySelectorAll(".question");
 
 questions.forEach((q) => {
     q.addEventListener("click", () => {
-        
-        const answer = q.nextElementSibling;
 
-  
+        const answer = q.nextElementSibling;
+        const arrow = q.querySelector(".arrow"); 
         if (answer.style.display === "block") {
             answer.style.display = "none";
+            arrow.style.transform = "rotate(0deg)";  
         } else {
             answer.style.display = "block";
+            arrow.style.transform = "rotate(180deg)"; 
         }
     });
 });
