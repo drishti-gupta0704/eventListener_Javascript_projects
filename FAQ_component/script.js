f5c691051c8e751ce93e0fd90eda704a37c3f0f5
drishti-gupta0704 <drishti.gupta070424@gmail.com>
2025-11-18 21:32:16 +0530

    adding functionality of opening one FAQ at a time

--- FAQ_component/script.js
+++ FAQ_component/script.js
@@ -1,16 +1,42 @@
+// const questions = document.querySelectorAll(".question");
+
+// questions.forEach((q) => {
+//     q.addEventListener("click", () => {
+
+//         const answer = q.nextElementSibling;
+//         const arrow = q.querySelector(".arrow"); 
+//         if (answer.style.display === "block") {
+//             answer.style.display = "none";
+//             arrow.style.transform = "rotate(0deg)";  
+//         } else {
+//             answer.style.display = "block";
+//             arrow.style.transform = "rotate(180deg)"; 
+//         }
+//     });
+// });
+
+// Select all FAQ questions
 const questions = document.querySelectorAll(".question");
+
 
 questions.forEach((q) => {
     q.addEventListener("click", () => {
+        questions.forEach((item) => {
+            if (item !== q) {
+                item.nextElementSibling.style.display = "none"; // closing other answers
+                item.querySelector(".arrow").style.transform = "rotate(0deg)"; // reset arrow
+            }
+        });
 
         const answer = q.nextElementSibling;
-        const arrow = q.querySelector(".arrow"); 
+        const arrow = q.querySelector(".arrow");
         if (answer.style.display === "block") {
             answer.style.display = "none";
-            arrow.style.transform = "rotate(0deg)";  
+            arrow.style.transform = "rotate(0deg)";
         } else {
             answer.style.display = "block";
-            arrow.style.transform = "rotate(180deg)"; 
+            arrow.style.transform = "rotate(180deg)";
         }
     });
 });
+
